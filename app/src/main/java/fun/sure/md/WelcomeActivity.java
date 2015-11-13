f76f2cca9e823807ceadbf49d6d0939c1f952ac1
wangshuo <wangshuo@fenbi.com>
2015-11-13 19:03:57 +0800

    ADD: recycler view & fab

--- app/src/main/java/fun/sure/md/WelcomeActivity.java
+++ app/src/main/java/fun/sure/md/WelcomeActivity.java
@@ -1,37 +1,162 @@
 package fun.sure.md;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.graphics.Palette;
+import android.support.v7.graphics.Palette.PaletteAsyncListener;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private CoordinatorLayout layoutContainer;
+    private FloatingActionButton fabView;
+    private CardView cardView;
+    private RecyclerView recyclerView;
+
+    // data
+    private List<String> listData;
+    private InnerAdapter adapter;
+
+    private static int[] resIds = {
+            R.mipmap.image_1,
+            R.mipmap.image_2,
+            R.mipmap.image_3,
+            R.mipmap.image_4,
+            R.mipmap.image_5
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initView();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_welcome, menu);
-        return true;
+    private void initView() {
+        layoutContainer = (CoordinatorLayout) findViewById(R.id.container);
+        fabView = (FloatingActionButton) findViewById(R.id.fab);
+        cardView = (CardView) findViewById(R.id.card);
+        recyclerView = (RecyclerView) findViewById(R.id.list);
+
+        fabView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getRandomIndex() % 2 == 0) {
+                    adapter.addData(1);
+                } else {
+                    adapter.removeData(1);
+                }
+            }
+        });
+
+        recyclerViewDemeo();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private int getRandomIndex() {
+        return new Random().nextInt(resIds.length);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void generatePalette(int i) {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resIds[i]);
+        if (bitmap != null) {
+            Palette.from(bitmap).generate(new PaletteAsyncListener() {
+                @Override
+                public void onGenerated(Palette palette) {
+                    fabView.setBackgroundColor(palette.getLightVibrantColor(android.R.color.transparent));
+                }
+            });
+        }
+    }
+
+    private List<String> getListData() {
+        if (listData == null) {
+            listData = new ArrayList<>();
+            for (int i = 'A'; i <= 'Z'; i++) {
+                listData.add(String.valueOf((char) i));
+            }
+        }
+        return listData;
+    }
+
+    /**
+     * Recycler View Demo
+     */
+    private void recyclerViewDemeo() {
+        adapter = new InnerAdapter();
+//        Linear
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    //region inner class
+
+    private class InnerAdapter extends RecyclerView.Adapter<InnerAdapter.InnerViewHolder> {
+
+        @Override
+        public InnerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(WelcomeActivity.this).inflate(R.layout.view_recycler_item, parent, false);
+            InnerViewHolder viewHolder = new InnerViewHolder(view);
+            return viewHolder;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void onBindViewHolder(InnerViewHolder holder, final int position) {
+            holder.textView.setText(getListData().get(position));
+            holder.textView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(layoutContainer, getListData().get(position), Snackbar.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return getListData().size();
+        }
+
+        public void addData(int position) {
+            getListData().add(position, "insert");
+            notifyItemInserted(position);
+        }
+
+        public void removeData(int position) {
+            getListData().remove(position);
+            notifyItemRemoved(position);
+        }
+
+        class InnerViewHolder extends ViewHolder {
+
+            TextView textView;
+
+            public InnerViewHolder(View itemView) {
+                super(itemView);
+                textView = (TextView) itemView.findViewById(R.id.text);
+            }
+        }
     }
+
+    //endregion
 }
